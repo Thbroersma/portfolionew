0d9da97ae6c9b5ee8bae39184f8864861fffd78b
Thierry Broersma <302677794@student.rocmondriaan.nl>
2021-11-25 09:11:37 +0100

    Start om te kijken of het werkt met php

---
+++ Templates/defaults/users/menu.php
@@ -0,0 +1,18 @@
+<nav class="navbar-style">
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="nav">
+                    <button class="switchButton">
+                        Switch up
+                    </button>
+                    <span><a href="index.html">😄</a></span>
+                    <div class="navigation">
+                        <a href="home" class="nav-right">Home</a>
+                        <a href="motivation" class="nav-right">Motivation</a>
+                        <a href="over" class="nav-right">About myself</a>
+                        <a href="game" class="nav-right">Games</a>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>

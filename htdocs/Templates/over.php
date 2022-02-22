90d41bbfe29de03eea9638acb8e27d18f8455bba
Thbroersma <70694558+Thbroersma@users.noreply.github.com>
2022-02-22 09:40:38 +0100

    bezig met het responsive maken van de website

--- htdocs/Templates/over.php
+++ htdocs/Templates/over.php
@@ -7,77 +7,79 @@
 <?php include_once("defaults/users/head.php") ?>
 
 <body class="overBody">
-<div class="nav-over">
 <?php
 
 include_once('defaults/users/menu.php');
 
 ?>
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-sm-6 col-md-12">
+		<h2 class="over"> About <i>my interests</i></h2>
+			
+			<p  class="text">
+				My interests are diverse as well so I will explain them below. Hopefully some of them will interest you. 
+			</p>
+		<div>
+			<ul class="text">
+				<li>Animals</li>
+				<li>Games</li>
+				<li>Nature</li>
+			</ul>
+			<br>
+		</div>
+		<img src="../public/img/nature.jpg" class="rounded nature">
+		<p class="text">
+			<b>Animals;</b>
+			"Animals are better than people", that is something I get from time to time. <br> When people let you down times again,
+			animals are there always for you to cheer you up. <br> That's why I would recommend everyone I they feel down look around 
+			<a href="image.php">here</a> or just <br> cuddle with one of the pets you know.
+			<br>
+			<br>
+			<b>Games;</b>
+			
+			can help me to get some frustration out and try to let go of things <br>
+			that worry me and it also makes me enjoy the game worlds that can be created. <br>
+			Just remember not to live in those world that are created but live in the world and make <br>it better, 
+			add the things that make it better for you and those around you.
+			<br>
+			<br>
+			<b>Nature</b> is one of the best things that I enjoy and it helps. Like when your <br>
+			head is too full with stuff and you can't get it back together, go on for <br>
+			a walk in nature. It can help you to calm your brain and your spirit. Pair it sometimes with easy <br>
+			music. It can refresh you in 20 to 30 minutes, can I say out of experience.
+		</p>
+		</div>
+		
+		
+		<div class="col-sm-6 col-md-12">
+		<h2 class="over">About <i>me</i></h2>
+		<p id="over">
+			<a href="https://www.linkedin.com/in/thierry-broersma-588174201/">Linkedin</a><br>
+			<a href="https://www.facebook.com/thierry.broersma">Facebook</a><br>
+			<a href="https://www.instagram.com/ThierryBroersma/?hl=nl">Instagram</a><br>
+			<a href="img/CV.pdf">CV</a><br>
+		</p>
+		<img src="../public/img/profiel.jfif" class="rounded high" alt="Me in my element">
+		<p class="text">
+			As you have already read on my website, I am a hard-working person who is commited to his goals. 
+			But I also like to enjoy the little things in life, like a relaxing walk through the forest or riding around on 
+			my bicycle. I like enjoy things where you are in your element or when you do something and you see someone  
+			else in their element. <br><br>For example, I like cycling a lot because you can enjoy the area around you, For example; 
+			one day I went for a ride of 150 km. There were a lot of different area's with forests, fields and 
+			the little towns. The fun part of it, is seeing the things and people but it is also a part of commitment. 
+			Because you can also go to these places by car and enjoy it anyway, but no, if you go by bicycle  
+			you are commited to see the whole journey. That is a big part of who I am: when I am commited to something I really go big 
+			and far for it. The work I put into this site, my job and the people around me. You could think: is this all 
+			the work you can put in it, but this is all the knowledge I have right now and I can put in it. There are a lot 
+			more things I would like to put in this website and that will come when I have the knowledge I need to make it work.
+		</p>
+		</div>
+		
+	</div>
 </div>
 
-	<aside class="aside-over">
-	</aside>
-	<main class="main-over">				
-	<h2 class="over"> About <i>my interests</i></h2>
-		
-		<p  class="text">
-			My interests are diverse as well so I will explain them below. Hopefully some of them will interest you. 
-		</p>
-	<div>
-		<ul class="text">
-			<li>Animals</li>
-			<li>Games</li>
-			<li>Nature</li>
-		</ul>
-		<br>
-	</div>
-	<img src="../public/img/nature.jpg" class="rounded nature">
-	<p class="text">
-		<b>Animals;</b>
-		"Animals are better than people", that is something I get from time to time. <br> When people let you down times again,
-		animals are there always for you to cheer you up. <br> That's why I would recommend everyone I they feel down look around 
-		<a href="image.php">here</a> or just <br> cuddle with one of the pets you know.
-		<br>
-		<br>
-		<b>Games;</b>
-		
-		can help me to get some frustration out and try to let go of things <br>
-		that worry me and it also makes me enjoy the game worlds that can be created. <br>
-		Just remember not to live in those world that are created but live in the world and make <br>it better, 
-		add the things that make it better for you and those around you.
-		<br>
-		<br>
-		<b>Nature</b> is one of the best things that I enjoy and it helps. Like when your <br>
-		head is too full with stuff and you can't get it back together, go on for <br>
-		a walk in nature. It can help you to calm your brain and your spirit. Pair it sometimes with easy <br>
-		music. It can refresh you in 20 to 30 minutes, can I say out of experience.
-	</p>
 	
-	</main>
-
-	<section>
-	<h2 class="over">About <i>me</i></h2>
-	<p id="over">
-		<a href="https://www.linkedin.com/in/thierry-broersma-588174201/">Linkedin</a><br>
-		<a href="https://www.facebook.com/thierry.broersma">Facebook</a><br>
-		<a href="https://www.instagram.com/ThierryBroersma/?hl=nl">Instagram</a><br>
-		<a href="img/CV.pdf">CV</a><br>
-	</p>
-	<img src="../public/img/profiel.jfif" class="rounded high" alt="Me in my element">
-	<p class="text">
-		As you have already read on my website, I am a hard-working person who is commited to his goals. 
-		But I also like to enjoy the little things in life, like a relaxing walk through the forest or riding around on 
-		my bicycle. I like enjoy things where you are in your element or when you do something and you see someone  
-		else in their element. <br><br>For example, I like cycling a lot because you can enjoy the area around you, For example; 
-		one day I went for a ride of 150 km. There were a lot of different area's with forests, fields and 
-		the little towns. The fun part of it, is seeing the things and people but it is also a part of commitment. 
-		Because you can also go to these places by car and enjoy it anyway, but no, if you go by bicycle  
-		you are commited to see the whole journey. That is a big part of who I am: when I am commited to something I really go big 
-		and far for it. The work I put into this site, my job and the people around me. You could think: is this all 
-		the work you can put in it, but this is all the knowledge I have right now and I can put in it. There are a lot 
-		more things I would like to put in this website and that will come when I have the knowledge I need to make it work.
-	</p>
-	</section>
 	<footer class="footer">
 		
 	</footer>
